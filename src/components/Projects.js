056502362463b87034805e89af6ed69c7dd7ab08
Luke Hook <lukehook@10-107-215-176.vlan895.client.wireless.uic.edu>
2022-08-29 13:12:49 -0500

    Add a project-container div to the projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,17 @@ const Projects = () => {
   return (
     <section>
       <h3>Projects</h3>
+      <div className="project-container">
+        <img src="" alt="null" />
+        <div>
+          <h4>App name</h4>
+          <p>description</p>
+          <div>
+            <button>Link</button>
+            <button>Respository</button>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
