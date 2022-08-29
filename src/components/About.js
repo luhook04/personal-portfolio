fbb9937db7a0850aab4cd635f966b86c18ebc6e1
Luke Hook <lukehook@10-107-215-176.vlan895.client.wireless.uic.edu>
2022-08-29 12:37:53 -0500

    Add the icons needed for the skills sections
    Include a span element that has the technology name.

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,56 @@ const About = () => {
           <img src="null" alt="" />
           <p>lorem ipsum dolor</p>
         </div>
-        <div className="skills">skills</div>
+        <div className="skills">
+          <div className="tech-icon">
+            <i class="devicon-html5-plain colored"></i>
+            <span>HTML</span>
+          </div>
+          <div className="tech-icon">
+            <i class="devicon-css3-plain colored"></i>
+            <span>CSS</span>
+          </div>
+          <div className="tech-icon">
+            <i class="devicon-javascript-plain colored"></i>
+            <span>JavaScript</span>
+          </div>
+          <div className="tech-icon">
+            <i class="devicon-react-original colored"></i>
+            <span>React</span>
+          </div>
+          <div className="tech-icon">
+            <i class="devicon-nodejs-plain colored"></i>
+            <span>Node.js</span>
+          </div>
+          <div className="tech-icon">
+            <i class="devicon-express-original colored"></i>
+            <span>Express</span>
+          </div>
+          <div className="tech-icon">
+            <i class="devicon-mongodb-plain colored"></i>
+            <span>MongoDB</span>
+          </div>
+          <div className="tech-icon">
+            <i class="devicon-heroku-plain colored"></i>
+            <span>Heroku</span>
+          </div>
+          <div className="tech-icon">
+            <i class="devicon-firebase-plain colored"></i>
+            <span>Firebase</span>
+          </div>
+          <div className="tech-icon">
+            <i class="devicon-git-plain colored"></i>
+            <span>Git</span>
+          </div>
+          <div className="tech-icon">
+            <i class="devicon-jest-plain colored"></i>
+            <span>Jest</span>
+          </div>
+          <div className="tech-icon">
+            <i class="devicon-webpack-plain colored"></i>
+            <span>Webpack</span>
+          </div>
+        </div>
       </div>
     </section>
   );
