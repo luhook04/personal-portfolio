9caf131934c5d4c0db6d6d9d74380d4d52af13d8
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-08-31 19:51:22 -0400

    Reset last commit

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,13 +1,15 @@
+import waldoGif from "../assets/wheres-waldo-gif.gif";
+
 const Projects = () => {
   return (
     <section>
       <h3>Projects</h3>
       <div className="project-container">
-        <img src="" alt="null" />
         <div>
           <h4>App name</h4>
           <p>description</p>
           <div>
+            <img src={waldoGif} alt="" />
             <button>Live Preview</button>
             <button>Respository</button>
           </div>
