215d90234f4a7e3827580a5d82ed3f824d0b083d
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-09-02 16:48:20 -0400

    Turn project container into a flexbox and fix the className typo in About component

--- src/components/About.js
+++ src/components/About.js
@@ -11,51 +11,51 @@ const About = () => {
         </div>
         <div className="skills">
           <div className="tech-icon">
-            <i class="devicon-html5-plain colored"></i>
+            <i className="devicon-html5-plain colored"></i>
             <span>HTML</span>
           </div>
           <div className="tech-icon">
-            <i class="devicon-css3-plain colored"></i>
+            <i className="devicon-css3-plain colored"></i>
             <span>CSS</span>
           </div>
           <div className="tech-icon">
-            <i class="devicon-javascript-plain colored"></i>
+            <i className="devicon-javascript-plain colored"></i>
             <span>JAVASCRIPT</span>
           </div>
           <div className="tech-icon">
-            <i class="devicon-react-original colored"></i>
+            <i className="devicon-react-original colored"></i>
             <span>REACT</span>
           </div>
           <div className="tech-icon">
-            <i class="devicon-nodejs-plain colored"></i>
+            <i className="devicon-nodejs-plain colored"></i>
             <span>NODEJS</span>
           </div>
           <div className="tech-icon">
-            <i class="devicon-express-original colored"></i>
+            <i className="devicon-express-original colored"></i>
             <span>EXPRESS</span>
           </div>
           <div className="tech-icon">
-            <i class="devicon-mongodb-plain colored"></i>
+            <i className="devicon-mongodb-plain colored"></i>
             <span>MONGODB</span>
           </div>
           <div className="tech-icon">
-            <i class="devicon-heroku-plain colored"></i>
+            <i className="devicon-heroku-plain colored"></i>
             <span>HEROKU</span>
           </div>
           <div className="tech-icon">
-            <i class="devicon-firebase-plain colored"></i>
+            <i className="devicon-firebase-plain colored"></i>
             <span>FIREBASE</span>
           </div>
           <div className="tech-icon">
-            <i class="devicon-git-plain colored"></i>
+            <i className="devicon-git-plain colored"></i>
             <span>GIT</span>
           </div>
           <div className="tech-icon">
-            <i class="devicon-jest-plain colored"></i>
+            <i className="devicon-jest-plain colored"></i>
             <span>JEST</span>
           </div>
           <div className="tech-icon">
-            <i class="devicon-webpack-plain colored"></i>
+            <i className="devicon-webpack-plain colored"></i>
             <span>WEBPACK</span>
           </div>
         </div>
