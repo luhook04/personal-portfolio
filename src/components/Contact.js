e43789ef4f31ae2c3f9dae999fa19df9bd25b2bb
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-09-08 13:03:08 -0400

    Add some icons and style them in the Contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,24 @@
 const Contact = () => {
   return (
-    <section id="contact">
+    <section id="contact" className="contact">
       <div className="flex-header">
         <div></div>
         <h1>Contact</h1>
         <div></div>
+      </div>
+      <div className="contact-content">
+        <a href="google.com">
+          <i className="devicon-github-original"></i>
+        </a>
+        <a href="google.com">
+          <i className="devicon-linkedin-plain"></i>
+        </a>
+        <a href="google.com">
+          <i className="fa-solid fa-envelope"></i>
+        </a>
+        <a href="google.com">
+          <i className="fa-solid fa-phone"></i>
+        </a>
       </div>
     </section>
   );
