07f28efbce277b8b4672af4806271bc281eaaa91
Luke Hook <lukehook04@gmail.com>
2023-03-09 00:06:51 -0500

    Make home title animation have better performance
    Remove excess paint flashes by creating a hidden div and changing it's
    opacity. This removes the need to have a text-shadow animation and makes
    the animation not trigger as many repaints.

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,53 +1,93 @@
 const Welcome = () => {
   return (
-    <section className="text-center flex h-screen">
+    <section className="text-center h-screen pt-52">
       <div className="text-slate-100 m-auto">
-        <div className="font-Welcome md:text-8xl text-7xl block glow-container">
-          <span className="animate-slide inline-block" style={{ '--i': 1 }}>
-            L
-          </span>
-          <span className="animate-slide2 inline-block" style={{ '--i': 2 }}>
-            U
-          </span>
-          <span className="animate-slide3 inline-block" style={{ '--i': 3 }}>
-            K
-          </span>
-          <span className="animate-slide4 inline-block" style={{ '--i': 4 }}>
-            E
-          </span>
-          <span
-            className="animate-slide4 inline-block text-amber-300"
-            style={{ '--i': 5 }}
-          >
-            H
-          </span>
-          <span
-            className="animate-slide3 inline-block text-amber-300"
-            style={{ '--i': 6 }}
-          >
-            O
-          </span>
-          <span
-            className="animate-slide2 inline-block text-amber-300"
-            style={{ '--i': 7 }}
-          >
-            O
-          </span>
-          <span
-            className="animate-slide inline-block text-amber-300"
-            style={{ '--i': 8 }}
-          >
-            K
-          </span>
+        <div className="font-Welcome md:text-8xl text-7xl glow-container w-full">
+          <div className="glow-text-animation">
+            <span className="animate-slide inline-block" style={{ '--i': 1 }}>
+              L
+            </span>
+            <span className="animate-slide2 inline-block" style={{ '--i': 2 }}>
+              U
+            </span>
+            <span className="animate-slide3 inline-block" style={{ '--i': 3 }}>
+              K
+            </span>
+            <span className="animate-slide4 inline-block" style={{ '--i': 4 }}>
+              E
+            </span>
+            <span
+              className="animate-slide4 inline-block text-amber-300"
+              style={{ '--i': 5 }}
+            >
+              H
+            </span>
+            <span
+              className="animate-slide3 inline-block text-amber-300"
+              style={{ '--i': 6 }}
+            >
+              O
+            </span>
+            <span
+              className="animate-slide2 inline-block text-amber-300"
+              style={{ '--i': 7 }}
+            >
+              O
+            </span>
+            <span
+              className="animate-slide inline-block text-amber-300"
+              style={{ '--i': 8 }}
+            >
+              K
+            </span>
+          </div>
+          <div className="glow-text-animation bright-text">
+            <span className="animate-slide inline-block" style={{ '--i': 1 }}>
+              L
+            </span>
+            <span className="animate-slide2 inline-block" style={{ '--i': 2 }}>
+              U
+            </span>
+            <span className="animate-slide3 inline-block" style={{ '--i': 3 }}>
+              K
+            </span>
+            <span className="animate-slide4 inline-block" style={{ '--i': 4 }}>
+              E
+            </span>
+            <span
+              className="animate-slide4 inline-block text-amber-300"
+              style={{ '--i': 5 }}
+            >
+              H
+            </span>
+            <span
+              className="animate-slide3 inline-block text-amber-300"
+              style={{ '--i': 6 }}
+            >
+              O
+            </span>
+            <span
+              className="animate-slide2 inline-block text-amber-300"
+              style={{ '--i': 7 }}
+            >
+              O
+            </span>
+            <span
+              className="animate-slide inline-block  text-amber-300"
+              style={{ '--i': 8 }}
+            >
+              K
+            </span>
+          </div>
         </div>
-        <div className="wrapper">
-          <ul className="words font-Change">
-            <li className="word">Software Engineer</li>
-            <li className="word">Technology Junkie</li>
-            <li className="word">Book Worm</li>
-            <li className="word">Indy Sports Fan</li>
-          </ul>
-        </div>
+      </div>
+      <div className="wrapper">
+        <ul className="words font-Change">
+          <li className="word">Software Engineer</li>
+          <li className="word">Technology Junkie</li>
+          <li className="word">Book Worm</li>
+          <li className="word">Indy Sports Fan</li>
+        </ul>
       </div>
     </section>
   );
