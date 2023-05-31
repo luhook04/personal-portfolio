84872802944580d9e4762e2cdaaf0b9a0c6673f2
Luke Hook <lukehook04@gmail.com>
2023-05-31 12:35:28 -0400

    Change the CSS for the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,15 @@ import logo from '../assets/LH12.png';
 
 const Header = () => {
   return (
-    <header>
-      <nav>
-        <div className="w-10/12 mx-auto flex justify-between text-slate-100">
+    <header className="py-3">
+      <nav className="my-auto">
+        <div className="w-10/12 mx-auto flex justify-between text-slate-800">
           <a href="#top">
             <img
               src={logo}
               title="Back to top"
               alt="Back to top"
-              className="md:h-36 md:w-36 w-28 h-28"
+              className="w-8 h-8"
             />
           </a>
           <ul className="flex md:text-lg items-center gap-x-7 sm:gap-x-10 md:gap-x-14 nav-links">
